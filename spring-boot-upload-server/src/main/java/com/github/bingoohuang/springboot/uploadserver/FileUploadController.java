76275378733b9c9b69a5b4ed767557fc74196667
bingoo <bingoo.huang@gmail.com>
2019-01-04 09:45:46 +0800

    file channel transfer (zero copy) to write file

--- spring-boot-upload-server/src/main/java/com/github/bingoohuang/springboot/uploadserver/FileUploadController.java
+++ spring-boot-upload-server/src/main/java/com/github/bingoohuang/springboot/uploadserver/FileUploadController.java
@@ -1,13 +1,17 @@
 package com.github.bingoohuang.springboot.uploadserver;
 
 
+import lombok.Cleanup;
 import lombok.SneakyThrows;
+import lombok.val;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.nio.channels.Channels;
 import java.util.UUID;
 
 @RestController
@@ -23,4 +27,18 @@ public class FileUploadController {
         return "ok";
     }
 
+    // Use file channel transfer (zero copy), this is almost half slower than direct file
+    // write when benchmarking with 5mb uploading.
+    @PostMapping("/upload0") @SneakyThrows
+    public String handleFileUpload0(@RequestParam("file") MultipartFile file) {
+        File tmpFile = new File("/tmp/springbootupload/" + UUID.randomUUID().toString());
+
+        @Cleanup val source = Channels.newChannel(file.getInputStream());
+        @Cleanup val destination = new FileOutputStream(tmpFile).getChannel();
+        destination.transferFrom(source, 0, file.getSize());
+
+        file.transferTo(tmpFile);
+        return "ok";
+    }
 }
+
