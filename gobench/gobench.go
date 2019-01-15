313865ca10798e19e2a354a20d1f62d1787d2d4c
bingoohuang <bingoo.huang@gmail.com>
2019-01-15 12:51:04 +0800

    refactor code

--- gobench/gobench.go
+++ gobench/gobench.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -65,36 +66,6 @@ type Result struct {
 	badFailed     int64
 }
 
-var readThroughput int64
-var writeThroughput int64
-
-// MyConn for net connection
-type MyConn struct {
-	net.Conn
-}
-
-// Read bytes from net connection
-func (myConn *MyConn) Read(b []byte) (n int, err error) {
-	len, err := myConn.Conn.Read(b)
-
-	if err == nil {
-		atomic.AddInt64(&readThroughput, int64(len))
-	}
-
-	return len, err
-}
-
-// Write bytes to net
-func (myConn *MyConn) Write(b []byte) (n int, err error) {
-	len, err := myConn.Conn.Write(b)
-
-	if err == nil {
-		atomic.AddInt64(&writeThroughput, int64(len))
-	}
-
-	return len, err
-}
-
 func init() {
 	flag.Int64Var(&requests, "r", -1, "Number of requests per client")
 	flag.IntVar(&clients, "c", 100, "Number of concurrent clients")
@@ -112,6 +83,39 @@ func init() {
 	flag.StringVar(&uploadFileName, "fn", "file", "Upload file name")
 
 	flag.Parse()
+
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	startTime := time.Now()
+	results := make(map[int]*Result)
+
+	HandleCtrlC(func() {
+		printResults(results, startTime)
+	})
+	HandleMaxProcs()
+
+	configuration, err := NewConfiguration()
+	if err != nil {
+		fmt.Println(err.Error())
+		flag.Usage()
+		return
+	}
+
+	fmt.Printf("Dispatching %d clients\n", clients)
+
+	var done sync.WaitGroup
+	done.Add(clients)
+	for i := 0; i < clients; i++ {
+		result := &Result{}
+		results[i] = result
+		go client(configuration, result, &done)
+	}
+
+	fmt.Println("Waiting for results...")
+	done.Wait()
+	printResults(results, startTime)
 }
 
 func printResults(results map[int]*Result, startTime time.Time) {
@@ -128,7 +132,6 @@ func printResults(results map[int]*Result, startTime time.Time) {
 	}
 
 	elapsed := int64(time.Since(startTime).Seconds())
-
 	if elapsed == 0 {
 		elapsed = 1
 	}
@@ -144,51 +147,16 @@ func printResults(results map[int]*Result, startTime time.Time) {
 	fmt.Printf("Test time:                      %10d sec\n", elapsed)
 }
 
-func readLines(path string) (lines []string, err error) {
-	var file *os.File
-	var part []byte
-	var prefix bool
-
-	if file, err = os.Open(path); err != nil {
-		return
-	}
-	defer file.Close()
-
-	reader := bufio.NewReader(file)
-	buffer := bytes.NewBuffer(make([]byte, 0))
-	for {
-		if part, prefix, err = reader.ReadLine(); err != nil {
-			break
-		}
-		buffer.Write(part)
-		if !prefix {
-			lines = append(lines, buffer.String())
-			buffer.Reset()
-		}
-	}
-	if err == io.EOF {
-		err = nil
-	}
-	return
-}
-
 // NewConfiguration create Configuration
-func NewConfiguration() *Configuration {
+func NewConfiguration() (*Configuration, error) {
 	if urlsFilePath == "" && urls == "" {
-		flag.Usage()
-		os.Exit(1)
+		return nil, errors.New("urls or urlsFilePath must be provided")
 	}
-
 	if requests == -1 && period == -1 {
-		fmt.Println("Requests or period must be provided")
-		flag.Usage()
-		os.Exit(1)
+		return nil, errors.New("requests or period must be provided")
 	}
-
 	if requests != -1 && period != -1 {
-		fmt.Println("Only one should be provided: [requests|period]")
-		flag.Usage()
-		os.Exit(1)
+		return nil, errors.New("only one should be provided: [requests|period]")
 	}
 
 	configuration := &Configuration{
@@ -197,7 +165,8 @@ func NewConfiguration() *Configuration {
 		postData:   nil,
 		keepAlive:  keepAlive,
 		requests:   requests,
-		authHeader: authHeader}
+		authHeader: authHeader,
+	}
 
 	if period != -1 {
 		configuration.period = period
@@ -221,7 +190,7 @@ func NewConfiguration() *Configuration {
 	}
 
 	if urlsFilePath != "" {
-		fileLines, err := readLines(urlsFilePath)
+		fileLines, err := FileToLines(urlsFilePath)
 
 		if err != nil {
 			log.Fatalf("Error in ioutil.ReadFile for file: %s Error: %v", urlsFilePath, err)
@@ -257,57 +226,9 @@ func NewConfiguration() *Configuration {
 	configuration.myClient.ReadTimeout = time.Duration(readTimeout) * time.Millisecond
 	configuration.myClient.WriteTimeout = time.Duration(writeTimeout) * time.Millisecond
 	configuration.myClient.MaxConnsPerHost = clients
-
 	configuration.myClient.Dial = MyDialer()
 
-	return configuration
-}
-
-// MyDialer create Dial function
-func MyDialer() func(address string) (conn net.Conn, err error) {
-	return func(address string) (net.Conn, error) {
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			return nil, err
-		}
-
-		myConn := &MyConn{Conn: conn}
-		return myConn, nil
-	}
-}
-
-// MustOpen open file successfully or panic
-func MustOpen(f string) *os.File {
-	r, err := os.Open(f)
-	if err != nil {
-		panic(err)
-	}
-	return r
-}
-
-// ReadUploadMultipartFile read file filePath for upload in multipart,
-// return multipart content, form data content type and error
-func ReadUploadMultipartFile(filename, filePath string) ([]byte, string, error) {
-	var buffer bytes.Buffer
-	writer := multipart.NewWriter(&buffer)
-
-	part, err := writer.CreateFormFile(filename, filepath.Base(filePath))
-	if err != nil {
-		return nil, "", err
-	}
-
-	file := MustOpen(filePath)
-	defer file.Close()
-
-	io.Copy(part, file)
-
-	writer.Close()
-
-	return buffer.Bytes(), writer.FormDataContentType(), nil
-}
-
-func init() {
-	rand.Seed(time.Now().UnixNano())
+	return configuration, nil
 }
 
 func randomImage() ([]byte, string, error) {
@@ -344,8 +265,6 @@ func client(configuration *Configuration, result *Result, done *sync.WaitGroup) 
 }
 
 func doRequest(configuration *Configuration, result *Result, tmpURL string) {
-	req := fasthttp.AcquireRequest()
-
 	method := configuration.method
 	postData := configuration.postData
 	contentType := configuration.contentType
@@ -355,6 +274,7 @@ func doRequest(configuration *Configuration, result *Result, tmpURL string) {
 		postData, contentType, _ = randomImage()
 	}
 
+	req := fasthttp.AcquireRequest()
 	req.SetRequestURI(tmpURL)
 	req.Header.SetMethodBytes([]byte(method))
 	req.Header.Set("Connection", IfElse(configuration.keepAlive, "keep-alive", "close"))
@@ -375,40 +295,53 @@ func doRequest(configuration *Configuration, result *Result, tmpURL string) {
 
 	if err != nil {
 		result.networkFailed++
-		return
-	}
-
-	if statusCode == fasthttp.StatusOK || statusCode == fasthttp.StatusCreated {
+	} else if statusCode == fasthttp.StatusOK || statusCode == fasthttp.StatusCreated {
 		result.success++
 	} else {
 		result.badFailed++
 	}
 }
 
-func main() {
-	startTime := time.Now()
-	results := make(map[int]*Result)
+var readThroughput int64
+var writeThroughput int64
 
-	HandleCtrlC(func() {
-		printResults(results, startTime)
-	})
-	HandleMaxProcs()
+// MyConn for net connection
+type MyConn struct {
+	net.Conn
+}
 
-	configuration := NewConfiguration()
+// Read bytes from net connection
+func (myConn *MyConn) Read(b []byte) (n int, err error) {
+	len, err := myConn.Conn.Read(b)
 
-	fmt.Printf("Dispatching %d clients\n", clients)
-
-	var done sync.WaitGroup
-	done.Add(clients)
-	for i := 0; i < clients; i++ {
-		result := &Result{}
-		results[i] = result
-		go client(configuration, result, &done)
+	if err == nil {
+		atomic.AddInt64(&readThroughput, int64(len))
 	}
 
-	fmt.Println("Waiting for results...")
-	done.Wait()
-	printResults(results, startTime)
+	return len, err
+}
+
+// Write bytes to net
+func (myConn *MyConn) Write(b []byte) (n int, err error) {
+	len, err := myConn.Conn.Write(b)
+
+	if err == nil {
+		atomic.AddInt64(&writeThroughput, int64(len))
+	}
+
+	return len, err
+}
+
+// MyDialer create Dial function
+func MyDialer() func(address string) (conn net.Conn, err error) {
+	return func(address string) (net.Conn, error) {
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			return nil, err
+		}
+
+		return &MyConn{Conn: conn}, nil
+	}
 }
 
 // HandleCtrlC handles Ctrl+C to exit after calling f.
@@ -436,4 +369,50 @@ func IfElse(condition bool, then, els string) string {
 	}
 
 	return els
+}
+
+// FileToLines read file into lines
+func FileToLines(filePath string) (lines []string, err error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	return
+}
+
+// MustOpen open file successfully or panic
+func MustOpen(f string) *os.File {
+	r, err := os.Open(f)
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
+
+// ReadUploadMultipartFile read file filePath for upload in multipart,
+// return multipart content, form data content type and error
+func ReadUploadMultipartFile(filename, filePath string) ([]byte, string, error) {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+
+	part, err := writer.CreateFormFile(filename, filepath.Base(filePath))
+	if err != nil {
+		return nil, "", err
+	}
+
+	file := MustOpen(filePath)
+	defer file.Close()
+
+	io.Copy(part, file)
+
+	writer.Close()
+
+	return buffer.Bytes(), writer.FormDataContentType(), nil
 }
