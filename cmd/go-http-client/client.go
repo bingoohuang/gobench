94f7ba3dc1b3a2f08cc486b47a593d7559084114
bingoohuang <bingoo.huang@gmail.com>
2019-12-31 13:21:30 +0800

    http client keep-alive

--- cmd/go-http-client/client.go
+++ cmd/go-http-client/client.go
@@ -17,23 +17,31 @@ func main() {
 	server := flag.String("server", "http://127.0.1:8812", "server get url")
 	sleep := flag.String("sleep", "1m", "sleep span")
 	retry := flag.Bool("retry", false, "retry if fail")
+	keepalive := flag.Bool("keepalive", true, "keep alive or not")
+
 	flag.Parse()
 
 	sleepSpan, err := time.ParseDuration(*sleep)
 	if err != nil {
 		fmt.Printf("fail to parse %s error %v\n", *sleep, err)
-		fmt.Printf("use default sleep=1m\n")
 
 		sleepSpan = 1 * time.Minute
 	}
 
 	var getFn func(url string) (resp *http.Response, err error)
 
+	log.Printf("server %v\n", *server)
+	log.Printf("sleep %v\n", sleepSpan)
+	log.Printf("keepalive %v\n", *keepalive)
+	log.Printf("retry mode %v\n", *retry)
+
 	if *retry {
-		log.Printf("retry(true) mode\n")
+		transport := cleanhttp.DefaultPooledTransport()
+		transport.DisableKeepAlives = !(*keepalive)
+		transport.MaxIdleConnsPerHost = -1
 
 		client := &retryablehttp.Client{
-			HTTPClient:   cleanhttp.DefaultPooledClient(),
+			HTTPClient:   &http.Client{Transport: transport},
 			RetryWaitMin: 1 * time.Second,
 			RetryWaitMax: 30 * time.Second,
 			RetryMax:     3,
@@ -43,10 +51,9 @@ func main() {
 
 		getFn = client.Get
 	} else {
-		log.Printf("retry(false) mode\n")
-
 		client := &http.Client{Transport: &http.Transport{
 			MaxIdleConnsPerHost: 1024,
+			DisableKeepAlives:   !*keepalive,
 		}}
 
 		getFn = client.Get
