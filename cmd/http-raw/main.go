cb42cb2812f30d6e3325750eeb47e6deda85e86c
bingoohuang <bingoo.huang@gmail.com>
2021-05-31 14:35:55 +0800

    http-raw/http-chunked

---
+++ cmd/http-raw/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bingoohuang/gg/pkg/flagparse"
+	"github.com/bingoohuang/gg/pkg/rest"
+	"github.com/bingoohuang/gg/pkg/ss"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	Addr   string `val:"127.0.0.1:5003" usage:"connecting address like 127.0.0.1:5003"`
+	Input  string `usage:"http request filename or direct input content"`
+	Server bool   `usage:"start as a server"`
+}
+
+func main() {
+	c := &Config{}
+	flagparse.Parse(c)
+
+	if c.Server {
+		c.server()
+	} else {
+		c.client()
+	}
+}
+
+func (c *Config) client() {
+	conn, err := net.Dial("tcp", c.Addr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close() // 关闭TCP连接
+
+	data, err := c.readInput()
+	if err != nil {
+		panic(err)
+	}
+
+	if err := c.send(conn, data); err != nil {
+		panic(err)
+	}
+}
+
+func (c *Config) send(conn net.Conn, data []byte) error {
+	//data := "OPTIONS * HTTP/1.1\r\nHost: localhost:5003\r\nContent-Length: 18\r\nContent-Type: application/json\r\n\r\n{\"name\": \"bingoo\"}"
+	s := strings.TrimSpace(strings.NewReplacer("\r\n", "\r\n", "\n", "\r\n").Replace(string(data)))
+	if v := FindContentLength(s); v == 0 {
+		s += "\r\n\r\n"
+	}
+
+	fmt.Printf("Request: === start ===\n%s\nRequest: === end ===\n", ss.Jsonify(s))
+
+	if _, err := conn.Write([]byte(s)); err != nil { // 发送数据
+		return err
+	}
+
+	buf := [512]byte{}
+	n, err := conn.Read(buf[:])
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(buf[:n]))
+
+	return nil
+}
+
+func (c *Config) readInput() ([]byte, error) {
+	data, err := ioutil.ReadFile(c.Input)
+	if err == nil {
+		return data, nil
+	}
+
+	if os.IsNotExist(err) {
+		return []byte(c.Input), nil
+	}
+
+	return data, err
+}
+
+func (c *Config) server() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rest.LogRequest(r, "all")
+	})
+
+	panic(http.ListenAndServe(c.Addr, mux))
+}
+
+var re = regexp.MustCompile(`(?i)Content-Length:\s*(\d+)`)
+
+func FindContentLength(s string) int {
+	contentLength := 0
+	if subs := re.FindStringSubmatch(s); len(subs) > 0 {
+		contentLength, _ = strconv.Atoi(subs[1])
+	}
+	return contentLength
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/Knetic/govaluate v3.0.0+incompatible
-	github.com/bingoohuang/gg v0.0.0-20210523100036-5c12dba40c7d
+	github.com/bingoohuang/gg v0.0.0-20210531061204-ec337d15a99f
 	github.com/bingoohuang/golang-trial v0.0.0-20210315044348-58fcdd21d9da
 	github.com/bingoohuang/jj v0.0.0-20210318135333-e12a222e0bad
 	github.com/cheggaaa/pb/v3 v3.0.7
